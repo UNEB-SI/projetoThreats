ee00ac22571d5cd76436a0ef9baf605a0d63cdce
Stephanie Lima <slrocha17@gmail.com>
2018-08-21 21:59:08 -0300

    corrigindo classificador

--- view/view.py
+++ view/view.py
@@ -169,23 +169,25 @@ class View(object):
     def classificador(self, file, tipo):
 
         if tipo == 'ameaca':
-            X = file[:, [2, 3]]
+            X = file.iloc[:, [2, 3]]
             y = file.columns
 
-            print(X)
-            exit()
-
         else:
-            X = file[:, [2, 3]]
+            X = file.iloc[:, [2, 3]]
             y = file.columns
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
         tree = DecisionTreeClassifier(criterion='entropy',
                                       max_depth=3, random_state=0)
+
+        print(tree)
+        exit()
         tree.fit(X_train, y_train)
 
         X_combined = np.vstack((X_train, X_test))
         y_combined = np.hstack((y_train, y_test))
+
+
 
         self.plot_decision_regions(X_combined, y_combined,
                               classifier=tree, test_idx=range(105, 150))
